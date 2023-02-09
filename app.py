ea0fba46bde01454bffae464e69c093fb4546289
monik <Alex.uma2007@yandex.ru>
2023-02-09 17:55:54 +0500

    task #4

--- app.py
+++ app.py
@@ -13,34 +13,46 @@ def index():
     return 'И на Марсе будут яблони цвести!'
 
 
-@app.route('/promotion')
+@app.route('/promotion_image')
 def promotion():
-    return """<!doctype html>
+    return f"""<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport">
     <title>Колонизация Марса</title>
+    <link rel="stylesheet" 
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+    crossorigin="anonymous">
+    <link rel="stylesheet" href={url_for('static', filename='css/style.css')}>
 </head>
 <body>
-    <p>Человечество вырастает из детства.</p>
+    <h1>Жди нас, Марс!</h1>
+    <img style="width: 33%;" src="{url_for('static', filename='img/mars.png')}">
+    <figcaption>Вот она какая, красная планета.</figcaption>
 
-    <p>Человечеству мала одна планета.</p>
+    <div class="alert alert-dark" role="alert">
+        Человечество вырастает из детства.
+    </div>
+    
+    <div class="alert alert-success" role="alert">
+        Человечеству мала одна планета.
+    </div>
+    
+    <div class="alert alert-dark" role="alert">
+        Мы сделаем обитаемыми безжизненные пока планеты.
+    </div>
+    
+    <div class="alert alert-warning" role="alert">
+        И начнем с Марса!
+    </div>
 
-    <p>Мы сделаем обитаемыми безжизненные пока планеты.</p>
-
-    <p>И начнем с Марса!</p>
-
-    <p>Присоединяйся!</p>
+    <div class="alert alert-danger" role="alert">
+        Присоединяйся!
+    </div>
 </body>
 </html>"""
-
-
-@app.route('/image_mars')
-def image():
-    return f'''<h1>Жди нас, Марс!</h1>
-    <img style="width: 33%;" src="{url_for('static', filename='img/mars.png')}">
-    <figcaption>Вот она какая, красная планета.</figcaption>'''
 
 
 if __name__ == '__main__':

c74d46bd710fbb0b3128008156581a609f0f7655
monik <Alex.uma2007@yandex.ru>
2023-02-16 20:45:51 +0500

    Lesson #2 task #3

--- Lesson #2/app.py
+++ Lesson #2/app.py
@@ -14,5 +14,12 @@ def training(prof):
     return render_template('training.html', prof=prof.lower())
 
 
+@app.route('/list_prof/<type_list>')
+def list_prof_route_handler(type_list):
+    list_prof = ['пилот', 'инженер', 'строитель', 'климатолог', 'астрогеолог', 'инженер-исследователь', 'штурман',
+                 'пилот дронов']
+    return render_template('list_prof.html', type_list=type_list, profs=list_prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')

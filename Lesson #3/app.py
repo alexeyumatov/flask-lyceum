2d179a97d119ba4f35be9d4dcd89d9338e73f32e
monik <Alex.uma2007@yandex.ru>
2023-03-13 16:33:36 +0500

    Lesson #4

---
+++ Lesson #3/app.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_user, login_required, logout_user
+from loginform import LoginForm
+from registerform import RegisterForm
+from data import db_session, users
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "yandexlyceum_supersecret_key"
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+def main():
+    db_session.global_init("db/mars.db")
+    app.run()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(users.User).get(user_id)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('base.html', title='Mars One')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register_page.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(users.User).filter(users.User.email == form.email.data).first():
+            return render_template('register_page.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = users.User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register_page.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(users.User).filter(users.User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login_page.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login_page.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    main()

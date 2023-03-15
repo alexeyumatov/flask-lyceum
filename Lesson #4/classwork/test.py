4ac7cc582a4fda8d7db166cb9a65586e9d7c0564
monik <Alex.uma2007@yandex.ru>
2023-03-16 00:24:22 +0500

    Lesson #4 homework task #2

--- Lesson #4/classwork/test.py
+++ Lesson #4/classwork/test.py
@@ -1,1 +1,10 @@
-# tests
+from requests import delete, get
+
+
+print('Delete with wrong id -\t', delete('http://127.0.0.1:5000/api/jobs/999').json())
+
+print('All data -\t', get('http://127.0.0.1:5000/api/jobs').json())
+
+print('Successful delete -\t', delete('http://127.0.0.1:5000/api/jobs/3').json())
+
+print('All data without deleted one -\t', get('http://127.0.0.1:5000/api/jobs').json())

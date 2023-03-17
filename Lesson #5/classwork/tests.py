d6c41a7bbd5c4d82bf99222f237176c9d5b3ff7e
monik <Alex.uma2007@yandex.ru>
2023-03-17 23:39:38 +0500

    Lesson #5 homework task #2 and #3

--- Lesson #5/classwork/tests.py
+++ Lesson #5/classwork/tests.py
@@ -1,1 +1,22 @@
-# tests
+from requests import get, post, delete
+
+print('Main get -\t', get('http://127.0.0.1:5000/api/v2/jobs').json())
+
+print('Get one job -\t', get('http://127.0.0.1:5000/api/v2/jobs/1').json())
+
+print('Incorrect get -\t', get('http://127.0.0.1:5000/api/v2/jobs/122').json())
+
+print("Successful post request -\t", post('http://127.0.0.1:5000/api/v2/jobs',
+                                          json={'team_leader': 1,
+                                                'job': 'TestJob post',
+                                                'work_size': 14,
+                                                'collaborators': '1, 2',
+                                                'is_finished': True}).json())
+
+print('Confirming post request -\t', get('http://127.0.0.1:5000/api/v2/jobs').json())
+
+print('Wrong post request -\t', post('http://127.0.0.1:5000/api/v2/jobs', json={}).json())
+
+print('Delete request -\t', delete('http://127.0.0.1:5000/api/v2/jobs/3').json())
+
+print('Wrong delete request -\t', delete('http://127.0.0.1:5000/api/v2/jobs/122').json())
